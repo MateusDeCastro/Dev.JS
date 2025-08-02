9016a5e279a17b5f118ba5b90001908902d6ff5c
Mateus de Castro Farah Aranha <148548338+MateusDeCastro@users.noreply.github.com>
2025-08-02 09:58:27 -0300

    Add files via upload

--- Quiz/main.js
+++ Quiz/main.js
@@ -1,7 +1,7 @@
 function vperg1(botaoClicado, opcaoSelecionada) {
     if (opcaoSelecionada == 'c1') {
         var respCerta = document.getElementById('c1');
-        respCerta.style.backgroundColor = "gree";
+        respCerta.style.backgroundColor = "green";
         document.getElementById('resp1').innerHTML = "✅Parabéns, você acertou!🎉🎊"
     }
     else {
@@ -23,7 +23,7 @@ function reiniciar1() {
 function vperg2(botaoClicado, opcaoSelecionada) {
     if (opcaoSelecionada == 'b2') {
         var respCerta = document.getElementById('b2');
-        respCerta.style.backgroundColor = "gree";
+        respCerta.style.backgroundColor = "green";
         document.getElementById('resp2').innerHTML = "✅Parabéns, você acertou!🎉🎊"
     }
     else {
@@ -40,4 +40,35 @@ function reiniciar2() {
     document.getElementById('c2').style.backgroundColor = "rgb(24, 2, 80)";
     document.getElementById('d2').style.backgroundColor = "rgb(24, 2, 80)";
     document.getElementById('resp2').innerHTML = "";
+}
+
+function vperg3(botaoClicado, opcaoSelecionada) {
+    if (opcaoSelecionada == 'a3') {
+        var respCerta = document.getElementById('a3');
+        respCerta.style.backgroundColor = "green";
+        document.getElementById('resp3').innerHTML = "✅Parabéns, você acertou!🎉🎊"
+    }
+    else {
+        var respCerta = document.getElementById('a3');
+        respCerta.style.backgroundColor = "green";
+        botaoClicado.style.backgroundColor = "red";
+        document.getElementById('resp3').innerHTML = "❌Resposta errada, que pena!"
+    }
+}
+
+function reiniciar3() {
+    document.getElementById('a3').style.backgroundColor = "rgb(24, 2, 80)";
+    document.getElementById('b3').style.backgroundColor = "rgb(24, 2, 80)";
+    document.getElementById('c3').style.backgroundColor = "rgb(24, 2, 80)";
+    document.getElementById('d3').style.backgroundColor = "rgb(24, 2, 80)";
+    document.getElementById('resp3').innerHTML = "";
+}
+
+function reiniciartudo() {
+    for (let i = 1; i <= 3; i++) {
+        ['a', 'b', 'c', 'd'].forEach(letra => {
+            document.getElementById(letra + i).style.backgroundColor = "rgb(24, 2, 80)";
+        });
+        document.getElementById('resp' + i).innerHTML = "";
+    }
 }
